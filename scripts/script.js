b5be0b21fdf8c212fb24b2125643b69416915eb7
Joseph Barsness <joseph.barsness@gmail.com>
2016-12-04 20:00:48 -0600

    updated some things

--- scripts/script.js
+++ scripts/script.js
@@ -6,11 +6,30 @@ console.log('start jq');
 // pushing student data from JSON object into an array that I can manipulate.
 var studentArray = [];
 
-$.ajax({
+var displayOnDom = function(){
+    console.log( 'studentArray', studentArray );
+    // loop through cars and update outputText
+    var outputText = '';
+    for (var i = 0; i < studentArray.length; i++) {
+      outputText += '<p>' + studentArray[i].first_name + ' ' + studentArray[i].last_name + ' ' + studentArray[i].info + '</p>';
+      outputText += '<img src="' + studentArray[i].picURL + '" />';
+}
+};
+var ajaxInfo = function(){
+  console.log('in ajaxInfo');
+  $.ajax({
       url: 'http://devjana.net/support/tau_students.json',
       dataType: 'JSON',
       success: function( data ){
         console.log( 'success, received:', data );
-      }    
-  });
+        console.log('data.tau: ', data.tau);
+        for (var i = 0; i < data.tau.length; i++) {
+          studentArray.push(data.tau[i]);
+        }
+      } // end console.log
+    }); // end ajax call
+  }; // end ajaxInfo function
+  displayOnDom();
+  ajaxInfo();
+  console.log(studentArray);
 }); // end document.ready
